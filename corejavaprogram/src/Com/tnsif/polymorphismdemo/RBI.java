21585cb7e307af1c684da94d4ff5eb80600e004b
punya <punya@LAPTOP-8PER7469>
2025-11-09 20:12:03 +0530

    first

---
+++ corejavaprogram/src/Com/tnsif/polymorphismdemo/RBI.java
@@ -0,0 +1,13 @@
+package Com.tnsif.polymorphismdemo;
+//demo for override
+//parent class
+
+public class RBI {
+	
+	float rateofinterest() {
+		return 5.9f;
+	}
+	static class punya{
+	}
+
+}
---
+++ corejavaprogram/src/Com/tnsif/polymorphismdemo/SBI.java
@@ -0,0 +1,10 @@
+package Com.tnsif.polymorphismdemo;
+
+public class SBI extends RBI {
+	
+	@Override
+	float rateofinterest() {
+		return 4.9f;
+	}
+
+}

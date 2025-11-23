ea6a081d238e42fcebebf1a07b566d0a47ace76a
punya <punya@LAPTOP-8PER7469>
2025-11-23 12:40:06 +0530

    first

---
+++ corejavaprogram/src/Com/tnsif/Scannerclassprograms/Taxcaluculation.java
@@ -0,0 +1,17 @@
+package Com.tnsif.Scannerclassprograms;
+
+public class Taxcaluculation {
+	
+	public void Caluculatetax(Person p) {
+		if(p.getIncome()<=1600) {
+			p.setTax(0);
+		}
+		else if(p.getIncome()>1600 && p.getIncome()<50000) {
+			p.setTax(5);
+		}
+		else {
+			p.setTax(10);
+		}
+	}
+
+}

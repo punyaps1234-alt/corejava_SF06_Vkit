f5a62b2a285cd2f746f5be424b91f68340d2168f
punya <punya@LAPTOP-8PER7469>
2025-11-23 12:55:50 +0530

    first

---
+++ corejavaprogram/src/Com/tnsif/abstractiondemo/hu.java
@@ -0,0 +1,17 @@
+package Com.tnsif.abstractiondemo;
+
+public class hu {
+	
+	public static void main(String[] args) {
+		int a=20;
+		int b=80;
+		int sum=add(a,b);
+		System.out.println(sum);
+	}
+
+	private static int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+}
